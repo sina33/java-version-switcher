23fd49d79aecfcceaae70645bef14c10354ef6d1
Sina Saeedi <Sina.Saeedi@tecnotree.com>
2019-08-26 17:26:50 +0430

    batch script added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-version-switcher
 Switch between installed java versions
+
+run java-switch.bat as administrator and choose between installed java versions
---
+++ java-switch.bat
@@ -0,0 +1,43 @@
+rem Set the JAVA_HOME environment variable and insert it into the system path.
+rem set JDK7="C:\Program Files\Java\jdk1.7.0_80"
+rem set JDK8="C:\Program Files\Java\jdk1.8.0_191"
+
+rem set JAVA_HOME="C:\Program Files\Java\jdk1.5.0_14"
+rem set PATH=%JAVA_HOME%\bin;%PATH%
+
+
+@echo off
+echo "Current Java Version is"
+echo %JAVA_HOME%
+setlocal enableDelayedExpansion
+SET PT="C:\Program Files\Java"
+cd %PT%
+::build "array" of folders
+set folderCnt=0
+for /f "eol=: delims=" %%F in ('dir /b /ad *') do (
+  set /a folderCnt+=1
+  set "folder!folderCnt!=%%F"
+)
+
+::print menu
+for /l %%N in (1 1 %folderCnt%) do echo %%N - !folder%%N!
+echo(
+ 
+:get selection
+set selection=
+set /p "selection=Enter number to Set the JAVA_HOME and PATH "
+echo you picked %selection% - !folder%selection%!
+cd %PT%\!folder%selection%!
+echo %cd%
+( endlocal & rem return
+   Set jdk=%cd%
+ 
+)
+echo Setting JAVA_HOME
+SETX /M JAVA_HOME "%jdk%"
+SET JAVA_HOME=%jdk%
+echo %JAVA_HOME%
+REM echo setting PATH
+REM setx PATH %JAVA_HOME%\bin;%PATH% -m
+REM echo Display java version
+REM java -version
